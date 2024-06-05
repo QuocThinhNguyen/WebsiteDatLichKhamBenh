7a0aa58b462855b8e322f8114f21f0a35c15c229
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2024-06-06 00:02:57 +0700

    edit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -43,10 +43,43 @@ let displayGetCRUD = async (req, res) => {
     });
 }
 
+let getEditCRUD = async (req, res) => {
+
+    let userId = req.query.id;
+    console.log(userId);
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+        // check userData not found
+        if (userData) {
+            return res.render("editCRUD.ejs", {
+                user: userData
+            })
+        }
+
+        return res.send("User find");
+    } else {
+        return res.send("User not found");
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    //return res.send("Update done!");
+
+    return res.render("displayCRUD.ejs", {
+        dataTable: allUsers
+    });
+    //updateUserData
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getTest: getTest,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
-    displayGetCRUD: displayGetCRUD
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
